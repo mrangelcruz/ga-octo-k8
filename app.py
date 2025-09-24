c462e0b58922a24b3bf5b4d33e2e530a52f6db23
Angel Cruz <mrangelcruz@gmail.com>
2025-09-24 13:31:35 -0500

    revise index.html to be dynamically set on bg color

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    bg_color = os.getenv("BG_COLOR", "#ffffff")  # default to white if not set
+    return render_template("index.html", bg_color=bg_color)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
